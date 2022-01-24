0a974e2cac16f1b512825158a1e079131fa7163a
Ebinisa Olorunninwo <35668327+oebinisa@users.noreply.github.com>
2022-01-24 02:03:59 +0100

    Update and rename ElasticBeanstalk.js to 0-ElasticBeanstalk.js

---
+++ 0-ElasticBeanstalk.js
@@ -0,0 +1,29 @@
+Elastic Beanstalk Walkthrough
+
+
+//Start with Cloud9
+A. Setup Cloud9 environment
+B. Initialize Git Repository
+C. 
+
+
+        
+        
+EOF
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
