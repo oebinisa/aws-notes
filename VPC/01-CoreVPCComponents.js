7929ab2bf7dab8b391b633f831af9e7778e7c527
Ebinisa Olorunninwo <35668327+oebinisa@users.noreply.github.com>
2022-02-06 01:35:26 +0100

    Create 01-CoreVPCComponents.js

---
+++ VPC/01-CoreVPCComponents.js
@@ -0,0 +1,17 @@
+VPC Components
+
+A. Create VPC
+
+
+Note:
+1. Always ensure you have your region set to us-east-1 (N. Virginia) before performing ops
+
+
+A. Create VPC
+1. AWS usually have a default VPC created
+2. Manually create a VPC
+
+
+
+
+
