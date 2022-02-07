3896b2317bf221da4ab05622efa8338273935c02
Ebinisa Olorunninwo <35668327+oebinisa@users.noreply.github.com>
2022-02-07 16:49:02 +0100

    Create 02-EC2-IAM-SG-NACL.js

---
+++ VPC/02-EC2-IAM-SG-NACL.js
@@ -0,0 +1,73 @@
+EC2 
+
+A. EC2 
+      Create EC2
+      IAM Role
+      Security Group (SG)
+B. Notes of SGs and NACLs
+
+
+A. EC2 
+1. Create EC2 Instances
+      Console => EC2 => Left Menu => Instances => Launch Instances
+            Chooce AMI: Amazon Linux 2
+            Instance Type: General Purpose - t2.micro
+            Configure Instance Details
+                  Number of Instaces: 1
+                  Network: Select preferred VPC
+                  Subnet: Select a (public) subnet
+                  IAM Role: Your IAM Role should have the following policies (create new Role if required)
+                        SSM: AmazonEC2RoleforSSM
+                        S3: AmazonS3FullAccess
+                  Advanced Details: User Data (Provide or upload startup script - optional) // See sample startup script below
+            Configure Security Group (SG)
+                  Select existing or create a new Security Group. To create a new SG
+                        Security Group Name: Enter a name
+                        Add Rule: Type - HTTP, Protocol - TCP, Port - 80, Source - My IP (restricts access to only my computer)
+                                  Type - SSH, Protocol - TCP, Port - 22, Source - My IP (restricts access to only my computer)
+            Key Pair: Use existing or generate ne Key Pair
+            
+2. Create another Instance for a Private Subnet.
+      Note: Configure Instance Details => Subnet: Ensure to select a Private Subnet
+            Configure Security Group => Add Rule: Only include "SSH" type (No HTTP)
+==================================================
+// The sample startup script below would install an Apache server and display a sample webpage
+// Sample Startup Script - userdata.sh
+--
+su ec2-user
+sudo yum unstall httpd -y
+sudo service httpd start
+sudo su -c 'cat > /var/www/httpd/index.html <<EOL
+<html>
+   <head>
+      <title>Call to Arms</title>
+      <style>
+         html, body { background: #000; padding: 0; margin: 0; }
+         img { display: block; margin: 0px auto; }
+      </style>
+   </head>
+   <body>
+      <img src='https://media.giphy.com/media/10YoCxWqM3NHxK/giphy.gif' height='100%'/>
+   </body>
+</html>     
+==================================================
+====================
+
+                  
+B. Notes on SG and NACL
+
+Security Groups
+1. SGs can only allow things, everything else is denied
+      E.g. You can (explicitly) set/allow inbound rules for SGs i.e My IP above - every other thing is implicitly denied
+      Outbound rules are just set to 0.0.0.0/0 (allow connection to the Internet)
+2. SGs are associated with EC2 Instances
+
+
+NACL
+1. NACLs on the other hand take both allow and deny rules
+      E.g. you can block specific IPs      
+2. NACLs are associated with Subnets     
+3. NACLs take precedence over SGs
+====================
+
+
